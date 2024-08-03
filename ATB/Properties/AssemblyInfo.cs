967df4e7dddb6fb342216ad1a804dfa20afc1da6
cheesegoldfish <32321566+cheesegoldfish@users.noreply.github.com>
2024-08-02 21:40:41 -0500

    Include AssemblyInfo with the deploy

--- ATB/Properties/AssemblyInfo.cs
+++ ATB/Properties/AssemblyInfo.cs
@@ -1,4 +1,12 @@
 ﻿using System.Reflection;
 
+[assembly: AssemblyTitle("ATB-cg")]
+[assembly: AssemblyDescription("a cheesy version of ATB")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATB-cg")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 [assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.1")]
