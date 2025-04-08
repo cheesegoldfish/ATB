cd419a5d7d7f1bc617677aaac641f7d3dcd60143
cheesegoldfish <32321566+cheesegoldfish@users.noreply.github.com>
2025-04-07 23:04:23 -0500

    Add current version to UI

--- ATB/ViewModels/SettingsViewModel.cs
+++ ATB/ViewModels/SettingsViewModel.cs
@@ -2,6 +2,7 @@
 using ATB.Commands;
 using ATB.Models;
 using ATB.Utilities;
+using System.IO;
 
 namespace ATB.ViewModels
 {
@@ -9,5 +10,26 @@ namespace ATB.ViewModels
     {
         public static MainSettingsModel Settings => MainSettingsModel.Instance;
         public static ICommand OverlayViewUpdate => new DelegateCommand(FormManager.OverlayToggle);
+
+        private string _version;
+        public string Version
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_version))
+                {
+                    var versionPath = Path.Combine(System.Environment.CurrentDirectory, @"BotBases\ATB\Version.txt");
+                    if (File.Exists(versionPath))
+                    {
+                        _version = File.ReadAllText(versionPath).Trim();
+                    }
+                    else
+                    {
+                        _version = "Version Unknown";
+                    }
+                }
+                return _version;
+            }
+        }
     }
 }
